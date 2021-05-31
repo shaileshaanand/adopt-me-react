b963920ebdea80c289cf123e8be2bafea033d75a
Shailesh Aanand <anaandshailu@gmail.com>
2021-05-31 10:28:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet.js";
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Luna"),
+    React.createElement("h3", {}, "Doge"),
+    React.createElement("h3", {}, "Havanese"),
+  ]);
+};
+
+export default Pet;
